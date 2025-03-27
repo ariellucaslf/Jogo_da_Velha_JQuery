b175261d12f78afa9b021fba71c0ff50116ca312
ariellucaslf <ariellucaslf@gmail.com>
2025-03-26 23:58:52 -0300

    fix: corrige no arquivo jogo.js

--- Jogo da Velha JQuerry/js/jogo.js
+++ Jogo da Velha JQuerry/js/jogo.js
@@ -1,132 +1,63 @@
 var rodada = 1;
-var matriz_jogo = Array(3);
+var matriz_jogo = {
+    'a': {1: 0, 2: 0, 3: 0},
+    'b': {1: 0, 2: 0, 3: 0},
+    'c': {1: 0, 2: 0, 3: 0}
+};
 
-matriz_jogo['a'] = Array(3);
-matriz_jogo['b'] = Array(3);
-matriz_jogo['c'] = Array(3);
+$(document).ready(function () {
 
-matriz_jogo['a'][1] = 0;
-matriz_jogo['a'][2] = 0;
-matriz_jogo['a'][3] = 0;
+    $('#btn_iniciar_jogo').click(function () {
 
-matriz_jogo['b'][1] = 0;
-matriz_jogo['b'][2] = 0;
-matriz_jogo['b'][3] = 0;
+        if ($('#entrada_apelido_jogador_1').val() == '' || $('#entrada_apelido_jogador_2').val() == '') {
+            alert('Preencha os apelidos dos jogadores.');
+            return false;
+        }
 
-matriz_jogo['c'][1] = 0;
-matriz_jogo['c'][2] = 0;
-matriz_jogo['c'][3] = 0;
+        $('#nome_jogador_1').html($('#entrada_apelido_jogador_1').val());
+        $('#nome_jogador_2').html($('#entrada_apelido_jogador_2').val());
 
-$(document).ready( function(){
+        $('#pagina_inicial').hide();
+        $('#palco_jogo').show();
+    });
 
-	$('#btn_iniciar_jogo').click( function(){
+    $('.jogada').click(function () {
+        var id_campo_clicado = this.id;
+        $("#" + id_campo_clicado).off();
+        jogada(id_campo_clicado);
+    });
 
-		//valida a digitação dos apelidos dos jogadores
-		if($('#entrada_apelido_jogador_1').val() == ''){
-			alert('Apelido do jogador 1 não foi preenchido');
-			return false;
-		}
+    function jogada(id) {
+        var icone = rodada % 2 === 1 ? 'url("images/marcacao_1.png")' : 'url("images/marcacao_2.png")';
+        var ponto = rodada % 2 === 1 ? -1 : 1;
 
-		if($('#entrada_apelido_jogador_2').val() == ''){
-			alert('Apelido do jogador 2 não foi preenchido');
-			return false;
-		}
+        rodada++;
 
-		//exibir os apelidos
-		$('#nome_jogador_1').html($('#entrada_apelido_jogador_1').val());
-		$('#nome_jogador_2').html($('#entrada_apelido_jogador_2').val());
+        $('#' + id).css('background-image', icone);
+        var linha_coluna = id.split('-');
+        matriz_jogo[linha_coluna[0]][linha_coluna[1]] = ponto;
 
-		//contrala visualização das divs
-		$('#pagina_inicial').hide();
-		$('#palco_jogo').show();
+        verifica_combinacao();
+    }
 
-	});
+    function verifica_combinacao() {
+        for (var i = 1; i <= 3; i++) {
+            ganhador(matriz_jogo['a'][i] + matriz_jogo['b'][i] + matriz_jogo['c'][i]);
+        }
 
-	$('.jogada').click( function(){
+        for (var linha of ['a', 'b', 'c']) {
+            ganhador(matriz_jogo[linha][1] + matriz_jogo[linha][2] + matriz_jogo[linha][3]);
+        }
 
-		var id_campo_clicado = this.id;
-		$("#"+ id_campo_clicado).off();
-		jogada(id_campo_clicado);
-	});
+        ganhador(matriz_jogo['a'][1] + matriz_jogo['b'][2] + matriz_jogo['c'][3]);
+        ganhador(matriz_jogo['a'][3] + matriz_jogo['b'][2] + matriz_jogo['c'][1]);
+    }
 
-	function jogada(id){
-		var icone = '';
-		var ponto = 0;
-
-		if((rodada % 2) == 1){
-			icone = 'url("images/marcacao_1.png")';
-			ponto = -1;
-		} else {
-			icone = 'url("images/marcacao_2.png")';
-			ponto = 1;
-		}
-		
-		rodada++;
-
-		$('#'+id).css('background-image', icone);
-
-		var linha_coluna = id.split('-');
-
-		matriz_jogo[linha_coluna[0]][linha_coluna[1]] = ponto;
-	
-		verifica_combinacao();
-
-	}
-
-	function verifica_combinacao(){
-
-		//verifica na horizontal
-		var pontos = 0;
-		for(var i = 1; i <= 3; i++){
-			pontos = pontos + matriz_jogo['a'][i];
-		}
-		ganhador(pontos);
-
-		pontos = 0;
-		for(var i = 1; i <= 3; i++){
-			pontos = pontos + matriz_jogo['b'][i];
-		}
-		ganhador(pontos);
-
-		pontos = 0;
-		for(var i = 1; i <= 3; i++){
-			pontos = pontos + matriz_jogo['c'][i];
-		}
-		ganhador(pontos);
-
-		//verifica na vertical
-		for(var l = 1; l <= 3; l++){
-			pontos = 0;
-			pontos += matriz_jogo['a'][l];
-			pontos += matriz_jogo['b'][l];
-			pontos += matriz_jogo['c'][l];
-
-			ganhador(pontos);
-		}
-
-		//verificar na diagonal
-		pontos = 0;
-		pontos = matriz_jogo['a'][1] + matriz_jogo['b'][2] + matriz_jogo['c'][3];
-		ganhador(pontos);
-
-		pontos = 0;
-		pontos = matriz_jogo['a'][3] + matriz_jogo['b'][2] + matriz_jogo['c'][1];
-		ganhador(pontos);
-
-	}
-
-	function ganhador(pontos){
-		if(pontos == -3){
-			var jogada_1 = $('#entrada_apelido_jogador_1').val();
-			alert(jogada_1 + ' é o vencedor');
-			$('.jogada').off();
-		
-		} else if(pontos == 3){
-			var jogada_2 = $('#entrada_apelido_jogador_2').val();
-			alert(jogada_2 + ' é o vencedor');
-			$('.jogada').off();
-		}
-	}
-
-
+    function ganhador(pontos) {
+        if (pontos === -3 || pontos === 3) {
+            var vencedor = pontos === -3 ? $('#entrada_apelido_jogador_1').val() : $('#entrada_apelido_jogador_2').val();
+            alert(vencedor + ' é o vencedor!');
+            $('.jogada').off();
+        }
+    }
 });
